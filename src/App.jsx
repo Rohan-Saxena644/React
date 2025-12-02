16ff327c25c58361d34184799a88ea061ee8230a
Rohan-Saxena644 <rohsax83@gmail.com>
2025-12-02 13:12:13 +0530

    My first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css"
+//import
+
+function Description(){
+  return <h3>I am the description</h3>
+}
 
 function App() {
-
-  return <h1>Hello World</h1>
-
+  return (                 //<button>Hello World</button>
+    <div>
+      <h1>This is my app component</h1>
+      <p>Inside app component we have the title component used</p>
+      <Title/>
+      <Description/>
+    </div>
+  );
 }
 
 export default App
