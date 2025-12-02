970873574a4a9a9bf859cd656c71adceddff20af
Rohan-Saxena644 <rohsax83@gmail.com>
2025-12-02 13:26:31 +0530

    learned import and exporting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css"
 //import
+import {Title} from "./Title.jsx";
 
 function Description(){
   return <h3>I am the description</h3>
--- src/Title.jsx
+++ src/Title.jsx
@@ -3,3 +3,4 @@ function Title(){
 }
 
 //export
+export  {Title};
