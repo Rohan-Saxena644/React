693fd45aa8b264fb383fc90a7a176fb79f7c1d80
Rohan-Saxena644 <rohsax83@gmail.com>
2025-12-03 21:40:08 +0530

    Rendering Arrays

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,11 +1,13 @@
 import Product from "./Product.jsx";
 
 function ProductTab(){
+  let options = ["hi-tech","durable" , "fast"];
+  let options2 = {a:"hi-tech", b: "durable" , c: "fast"};
     return (                 
         <>
-          <Product title="phone" price={30000}/>
-          <Product title="laptop" price={40000}/>
-          <Product title="pen"/>
+          <Product title="phone" price={30000} features={options} features2={options2}/>
+          <Product title="laptop" price={40000} features={options} features2={options2}/>
+          <Product title="pen" price={1} features={options} />
         </>
     );
 }
