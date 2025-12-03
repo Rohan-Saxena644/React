41b0431da79bf4f58e975fd0dc5fd27c8d0cc89c
Rohan-Saxena644 <rohsax83@gmail.com>
2025-12-03 12:27:00 +0530

    Added structured components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import "./App.css"
 //import
 import {Title} from "./Title.jsx";
+import Product from "./Product.jsx";
+import ProductTab from "./ProductTab.jsx";
 
 function Description(){
   return <h3>I am the description</h3>
 }
 
 function App() {
-  return (                 //<button>Hello World</button>
-    <>
-      <h1>This is my app component</h1>
-      <p>Inside app component we have the title component used</p>
-      <Title/>
-      <Description/>
-    </>
+  return (                 
+    <ProductTab/>
   );
 }
 
-export default App
+export default App;
