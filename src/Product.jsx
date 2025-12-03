5bfaf7d9ac4cffd262f9cbab3ce9574a0af1a84b
Rohan-Saxena644 <rohsax83@gmail.com>
2025-12-03 23:02:22 +0530

    Learned React conditionals and Dynamic Component Styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,19 @@
 import "./Product.css";
 
-function Product({title , price , features , features2}){
+function Product({title , price , features}){
+    let isDiscount = price>30000;
+    let styles = {backgroundColor: isDiscount ? "yellow" : ""};
+
     return (
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
-            <p>{features}</p>
-            <p>{features2?.a}</p> 
+            <ul>{features.map((feature) => <li>{feature}</li>)}</ul>
+            {isDiscount && <p>Discount of 5%</p>}
+            {/* <ul>{features}</ul> */}
+            {/* <p>{features2?.a}</p>  */}
         </div>
-    )
+    );
 } 
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,12 +1,12 @@
 import Product from "./Product.jsx";
 
 function ProductTab(){
-  let options = ["hi-tech","durable" , "fast"];
-  let options2 = {a:"hi-tech", b: "durable" , c: "fast"};
+  let options = ["hi-tech","durable","fast"];
+  // let options2 = {a:"hi-tech", b: "durable" , c: "fast"};
     return (                 
         <>
-          <Product title="phone" price={30000} features={options} features2={options2}/>
-          <Product title="laptop" price={40000} features={options} features2={options2}/>
+          <Product title="phone" price={30000} features={options}/>
+          <Product title="laptop" price={40000} features={options}/>
           <Product title="pen" price={1} features={options} />
         </>
     );
