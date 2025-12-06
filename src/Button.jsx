2887f0f4c5d28d924361e8c2e67e5dc971b1a8aa
Rohan-Saxena644 <rohsax83@gmail.com>
2025-12-06 23:28:55 +0530

    Event listners through react

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,15 +1,21 @@
-function printHello(){
+function printHello(event){
     console.log("Hello!");
+    console.log(event);
 }
 
 function printBye(){
     console.log("bye!");
 }
 
+function handleDblClick(){
+    console.log("you double clicked");
+}
+
 export default function Button(){
     return (
     <div>
         <button onClick={printHello}>Click Me!</button>
-        <p onClick={printBye}>This parah is for event demo</p>
-    </div>)
+        <p onMouseOver={printBye}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae doloribus nostrum quia dicta sequi, quisquam, debitis hic fugiat cupiditate, voluptate commodi excepturi praesentium tempore quasi magni alias dolorum iusto sit?</p>
+        <button onDoubleClick={handleDblClick}>double click me!</button>
+    </div>);
 }
